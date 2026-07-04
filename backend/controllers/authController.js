b0a3930b81d416237a260e47681d929df6de2e38
Aman <jharliaman@gmail.com>
2026-07-04 21:39:58 +0530

    AuthController

---
+++ backend/controllers/authController.js
@@ -0,0 +1,64 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+// Generate JWT Token
+const generateToken = (User) => {
+  return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
+// Register User
+
+export const registerUser = async (req, res) => {
+  try {
+    const { email, password, role } = req,
+      body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      email,
+      password: hashedPassword,
+      role,
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role),
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Login User
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ message: 'User is not registered' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    res.json({
+      _id: user._id,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id, user.role)
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
