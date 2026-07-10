1487d611d39292e2424a125e397d441605ab9881
Aman <jharliaman@gmail.com>
2026-07-10 17:34:44 +0530

    candidateRoute, server

---
+++ backend/routes/candidateRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+const router=express.Router();
+import {protect,authorize} from "../middlewares/authMiddleware.js"
+
+import {
+    createCandidate,
+  getCandidates,
+  getCandidateById,
+  updateCandidateStatus,
+  updateCandidate,
+  deleteCandidate
+} from "../controllers/candidateConroller.js"
+
+
+router.post('/', protect, authorize('admin', 'hr'), createCandidate);
+router.get('/', protect, authorize('admin', 'hr'), getCandidates);
+router.get('/:id', protect, authorize('admin', 'hr'), getCandidateById);
+router.put('/:id', protect, authorize('admin', 'hr'), updateCandidate);
+router.put('/:id/status', protect, authorize('admin', 'hr'), updateCandidateStatus);
+router.delete('/:id', protect, authorize('admin'), deleteCandidate);
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import employeeRoutes from "./routes/employeeRoutes.js"
+import departmentRoutes from "./routes/departmentRoutes.js"
+import jobPositionRoutes from "./routes/jobPositionRoutes.js"
+import attendanceRoutes from "./routes/attendanceRoutes.js"
+import leaveRoutes from "./routes/leaveRoutes.js"
+import candidateRoutes from "./routes/candidateRoutes.js"
+import payrollRoutes from "./routes/payrollRoutes.js"
+import cors from "cors"
 
 dotenv.config();
 
@@ -10,11 +18,30 @@ connectDB();
 const app=express();
 
 
+app.use(cors());
 app.use(express.json());    // This allows Express to read JSON data from the request body.
 app.use(express.urlencoded({ extended: true }));   // This allows Express to read form data (HTML forms).
 
 app.use("/api/auth", authRoutes);
+app.use('/api/employees', employeeRoutes);
+app.use('/api/departments', departmentRoutes);
+app.use('/api/job-positions', jobPositionRoutes);
+app.use('/api/attendance', attendanceRoutes);
+app.use('/api/leaves', leaveRoutes);
+app.use('/api/candidates', candidateRoutes);
+app.use('/api/payroll', payrollRoutes);
 
+
+// 404 handler — for routes that don't exist
+app.use((req, res) => {
+  res.status(404).json({ message: "Route not found" });
+});
+
+// Global error handler — catches unexpected errors anywhere in the app
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something went wrong on the server" });
+});
 
 const PORT=process.env.PORT || 5000;
 
